67a05440f3e5509e8adcf9d7a43c4946e26bbe15
Brogrammer7 <ct10025@tutanota.com>
2026-07-07 22:49:39 -0700

    Write ViewModel logic to make network call and populate model

--- app/src/main/java/com/restest/flashcards/views/composables/CardsViewModel.kt
+++ app/src/main/java/com/restest/flashcards/views/composables/CardsViewModel.kt
@@ -2,11 +2,14 @@ package com.restest.flashcards.views.composables
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.restest.flashcards.data.model.CardResponse
 import com.restest.flashcards.data.model.FlashCard
 import com.restest.flashcards.data.repository.CardsLocalRepository
+import com.restest.flashcards.network.ResApi
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
@@ -19,9 +22,29 @@ class CardsViewModel(
         MutableStateFlow(listOf<FlashCard>())
     val cardList = _cardList.asStateFlow()
 
+    private val _onlineCardsList = MutableStateFlow(CardResponse())
+    val onlineCardsList: StateFlow<CardResponse> = _onlineCardsList.asStateFlow()
+
     init {
         collectRepositoryEvents()
         getFlashCards()
+        getOnlineCards()
+    }
+
+    fun getOnlineCards() {
+        viewModelScope.launch {
+            try {
+                val response = ResApi.retrofitService.getFlashCards()
+
+                if (response.isSuccessful) {
+                    response.body()?.let { cardResponse ->
+                        _onlineCardsList.value = cardResponse
+                    }
+                }
+            } catch (e: Exception) {
+
+            }
+        }
     }
 
     private fun collectRepositoryEvents() {
